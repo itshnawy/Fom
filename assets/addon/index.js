7e4ce2e95effb4f2391efe479b01d4f96ac0a979
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-03-12 01:28:56 +0200

    add dark modal support

--- assets/addon/index.js
+++ assets/addon/index.js
@@ -1,6 +1,6 @@
 // Append CDN
 var script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/gh/zerodevx/zero-md@2/dist/zero-md.min.js';
+script.src = './assets/addon/zero-md.min.js';
 script.type = "module"
 document.head.appendChild(script);
 
@@ -16,17 +16,22 @@ function mdModal(
     // Create modal container
     const modalContainer = document.createElement("div");
     modalContainer.classList.add("mdModal");
-    document.head.innerHTML += `<style id="mdStyle">
-.mdModal {background: ${bg !== "#000" && bg !== "#000000" ? bg : "#fff"}; }
-    </style>`
+    // Add modal styles
+
+    const style = document.createElement("style");
+    style.id = "mdStyle";
+    style.innerHTML = `.mdModal {background: ${bg};}`;
+    document.head.appendChild(style);
+
     // Modal content
     modalContainer.innerHTML = `
-    <button id="close-btn" title="Close Button">✖</button><h1 class="modalheader">${header}</h1>
+    <button id="close-btn" title="Close Button" style="color:${bg === "#000" || bg === "#000000" ? '#fff' : '#000'}">✖</button>
+    <h1 class="modalheader" style="color:${bg === "#000" || bg === "#000000" ? '#fff' : '#000'}">${header}</h1>
     <zero-md id="mdContainer" src="${src}" dir="${dir}">
             <template>
-                <link rel="stylesheet" href="/assets/addon/md.css" />
+                <link id="mainmdstyle" rel="stylesheet" href="${bg === "#000" || bg === "#000000" ? '/assets/addon/md-dark.css' : '/assets/addon/md.css'}" />
                 <link rel="stylesheet" href="/assets/addon/hili.css" />
-                <style id="mdStyle">
+                <style>
     @import url('${fontSrc}');
     * {
         font-family: "${fontName}";
@@ -34,7 +39,6 @@ function mdModal(
     </style>
             </template>
         </zero-md>`;
-        
     // Append modal container to the body
     document.body.appendChild(modalContainer);
 
@@ -43,9 +47,18 @@ function mdModal(
 
     // Event listener for closing the modal
     const closeButton = modalContainer.querySelector("#close-btn");
+    if (closeButton) {  
     closeButton.addEventListener("click", function() {
+
         document.body.style.overflow = "auto";
-        document.getElementById("mdStyle").remove();
+        const mdStyle = document.getElementById("mdStyle");
+        if (mdStyle) mdStyle.remove();
         modalContainer.remove();
-    });
+    })
+} else {
+    console.error("Close button not found.");
+};
+
+
+
 }
