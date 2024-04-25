e410953b35a628eaa9af80b3ffb49144fd696080
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-04-25 20:36:24 +0200

    Agree Feature
    Implemented feature to preview rules using mdModal. Your users can now review and accept rules by clicking a designated button, which automatically closes the modal

--- mdModal.js
+++ mdModal.js
@@ -78,6 +78,61 @@ const mdModal = {
                 }
         }
 },
+    agree: function(src,headerText,buttonText) {
+                // Parameter Validation
+                if (typeof src !== 'string' || src.trim() === '') {
+                    throw new Error('src parameter must be a non-empty string.');
+                    }
+            
+                    // Vars
+                    let p = document.body;
+                    let ov = document.createElement("div");
+                    let modal = document.createElement("div"); modal.id = "modal";
+                    let BGTL = this.currentColor === "#000" || this.currentColor === "#000000" ? '#ffffff' : '#000000';
+                    let dir = this.currentLanguage === 'ar' ? "rtl" : "ltr";
+                    let dOrW = this.currentColor === "#000" || this.currentColor === "#000000" ? './assets/css/md-dark.css' : './assets/css/md.css';
+                    ov.id = "overlay";
+                    const modalContainer = document.createElement("div");
+                    modalContainer.classList.add("mdModal");
+                    modalContainer.style.backgroundColor = this.currentColor;
+            
+            
+                    // append
+                    loadCSS(this.style)
+                    loadJS("./assets/js/zero-md.min.js", "module")
+            
+                    // Modal Content
+                    modalContainer.innerHTML = `
+                    <button id="close-btn" title="Close Button" style="color:${BGTL}">&times;</button>
+                    <div class="header" style="color:${BGTL}"><h1>${headerText}</h1></div>
+                    <zero-md id="mdContainer" src="${src}" dir="${dir}">
+                     <template>
+                      <link rel="stylesheet" href="${dOrW}" />
+                      <link rel="stylesheet" href="./assets/css/hili.css" />
+                     </template>
+                    </zero-md>
+                    <div class="footer agree" style="color:${BGTL}"><button id="agreeColse">${buttonText}</button></div>
+                    `;
+                    
+                    modal.append(modalContainer)
+                    // modal opening animation
+                    const modalOpening = [{transform: "scale(0)"},{transform: "scale(1)"},];
+                    const modalOpeningTiming = {duration: 210,iterations: 1,};
+                    // append on the position
+                    p.appendChild(ov)
+                    p.appendChild(modal);
+                    modalContainer.animate(modalOpening, modalOpeningTiming)
+                    document.body.style.overflow = "hidden";
+                    let m = modalContainer.querySelector("#mdContainer")
+                    m.classList.add("m");
+            
+                // closing
+                const closeButton = modalContainer.querySelector("#close-btn");
+                const agree = modalContainer.querySelector("#agreeColse");
+                closeButton.addEventListener("click", () => close(ov,modal,modalContainer))
+                ov.addEventListener('click', () => close(ov,modal,modalContainer))
+                agree.addEventListener('click', () => close(ov,modal,modalContainer))
+    }
     
 }
 
